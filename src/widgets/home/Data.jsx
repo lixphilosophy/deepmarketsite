1ffd7732b02b2bd6852e86781ff7beea140ab7e0
Martin Li <lixphilosophy@gmail.com>
2019-03-13 13:04:33 -0700

    increase tensorflow font-size

--- src/widgets/home/Data.jsx
+++ src/widgets/home/Data.jsx
@@ -1,6 +1,6 @@
 const Data = {
     "title": `DeepMarket`,
-    "subtitle": `An Edge Computing Marketplace with Distributed Tensorflow Execution Capability`,
+    "subtitle": `An Edge Computing Marketplace with Distributed <b style="font-size: 2.1rem;">Tensorflow</b> Execution Capability`,
     "bgImage": `home-bg.jpg`,
     "bgImagePlaceholder": `home-bg-placeholder.jpg`
 }
