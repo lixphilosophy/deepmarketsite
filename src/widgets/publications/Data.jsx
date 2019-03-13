d116a480ce5750a7191140bcd8ebf481d7c4d5cc
Martin Li <lixphilosophy@gmail.com>
2019-03-13 16:17:39 -0700

    add publications

--- src/widgets/publications/Data.jsx
+++ src/widgets/publications/Data.jsx
@@ -1,5 +1,7 @@
 const Data = {
-    "article": [],
+    "article": [
+        [`<i>S. Yerabolu, S. Kim, S. Gomena, X. Li, R. Patel, S. Bhise, and E. Aryafar,</i> “<a href="static/media/2019-ECOFEC-DeepMarket.c2bc949b.pdf" target="_blank">DeepMarket: An Edge Computing Marketplace with Distributed TensorFlow Execution Capability</a>,” in IEEE ECOFEC (ECOnomics of Fog, Edge, and Cloud Computing) Workshop 2019, Paris, France, April 2019.`, 'article-para pt-2']
+    ],
     "title" : "DeepMarket | Publications",
     "pageTitle": "Publications",
     "imageIndex": []
