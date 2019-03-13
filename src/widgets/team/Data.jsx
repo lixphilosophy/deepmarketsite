938862b6f7cd3a53a69ec802b3c39e686fce5ef7
Martin Li <lixphilosophy@gmail.com>
2019-03-13 16:34:11 -0700

    modified team info/fixed text align

--- src/widgets/team/Data.jsx
+++ src/widgets/team/Data.jsx
@@ -1,14 +1,14 @@
 const Data = [
-  {name: `<a target="_blank" href="http://web.cecs.pdx.edu/~aryafare/">Dr. Ehsan Aryafar</a>`, position: "Assistant Professor", desc: "", img: "aryafar.jpg" },
-  {name: "Dr. Zhenming Liu", position: "Assistant Professor", desc: "", img: "zhenming.jpg" },
-  {name: "Dr. Carlee Joe-Wong", position: "Assistant Professor", desc: "", img: "carlee.jpg" },
-  {name: "Xiaoxi Zhang", position: "Research Assistant", desc: "Postdoc, Carnegie Mellon University", img: "xiaoxi.jpg" },
-  {name: "Xuanzhe (Martin) Li", position: "Research Assistant, Frontend Developer", desc: "Undergraduate, Portland State University", img: "martin.jpg" },
-  {name: "Samuel Gomena", position: "Research Assistant, System Architect", desc: "Undergraduate, Portland State University", img: "sam.jpg" },
-  {name: "Soyoung Kim", position: "Research Assistant, Performance Monitoring", desc: "Graduate, Portland State University", img: "kim.jpg" },
-  {name: "Amarjit Arvind Jadhav", position: "Research Assistant, System Security", desc: "Graduate, Portland State University", img: "amarjit.jpg" },
-  {name: "Shraddha Bhise", position: "Research Assistant, System Security", desc: "Graduate, Portland State University", img: "shraddha.jpg" },
-  {name: "Rohan Patel", position: "Research Assistant, Backend Developer", desc: "Graduate, Portland State University", img: "rohan.jpg" },
+  {name: `<a target="_blank" href="http://web.cecs.pdx.edu/~aryafare/">Prof. Ehsan Aryafar</a>`, position: "Assistant Professor", desc: "Portland State University", img: "aryafar.jpg" },
+  {name: `<a target="_blank" href="http://zmliu.org/">Prof. Zhenming Liu</a>`, position: "Assistant Professor", desc: "The College of William & Mary", img: "zhenming.jpg" },
+  {name: `<a target="_blank" href="https://www.andrew.cmu.edu/user/cjoewong/">Prof. Carlee Joe-Wong</a>`, position: "Assistant Professor", desc: "Carnegie Mellon University", img: "carlee.jpg" },
+  {name: "Dr. Xiaoxi Zhang", position: "Post-Doctoral Researcher", desc: "Carnegie Mellon University", img: "xiaoxi.jpg" },
+  {name: "Xuanzhe (Martin) Li", position: "Frontend Developer", desc: "Undergraduate, Portland State University", img: "martin.jpg" },
+  {name: "Samuel Gomena", position: " System Architect", desc: "Undergraduate, Portland State University", img: "sam.jpg" },
+  {name: "Soyoung Kim", position: "Performance Monitoring", desc: "Graduate, Portland State University", img: "kim.jpg" },
+  {name: "Amarjit Arvind Jadhav", position: "System Security", desc: "Graduate, Portland State University", img: "amarjit.jpg" },
+  {name: "Shraddha Bhise", position: "System Security", desc: "Graduate, Portland State University", img: "shraddha.jpg" },
+  {name: "Rohan Patel", position: "Backend Developer", desc: "Graduate, Portland State University", img: "rohan.jpg" },
 ]
 
 const HeadTitle = "DeepMarket | Team"
