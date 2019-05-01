cbc8678ba2e8a953a5bdef5cdccc2ca21b96d5d6
Martin <lixphilosophy@gmail.com>
2019-04-30 23:58:56 -0700

    move dev env to martin machine (update purpose). Add new about paper

--- src/widgets/news/Data.jsx
+++ src/widgets/news/Data.jsx
@@ -1,8 +1,10 @@
 const Data = {
     "article": [
+        [`<b>April, 2019</b> - We presented our first paper (DeepMarket: An Edge Computing Marketplace with Distributed TensorFlow Execution Capability) at the First International IEEE Workshop on the Economics of Fog, Edge and Cloud Computing (ECOFEC 2019), in Paris, France. The paper was well received by the community and we are looking forward to expanding DeepMarket’s network to include other universities and research groups.`, 'article-para'],
+        
         [`<b>February, 2019</b> - Susham Yerabolu (CS Master student at Portland State University) was the first student who contributed to DeepMarket. He has accepted a job offer from Informatica (Austin, TX) and will be starting his new job in Feb 2019. We appreciate all his contributions to the project and wish him well in his career!`, 'article-para py-3'],
     
-        [`<b>August, 2018</b> - We hosted a high school student, Patrick Huang, from Southridge High School in our lab in summer 2018. Patrick contributed to several projects including the initial design of the DeepMarket website! We have posted new summer internship opportunities (for summer 2019) for high school students through collaboration with Saturday Academy, Apprenticeship in Science and Engineering program.`, 'article-para'],
+        [`<b>August, 2018</b> - We hosted a high school student, Patrick Huang, from Southridge High School in our lab in summer 2018. Patrick contributed to several projects including the initial design of the DeepMarket website! We have posted new summer internship opportunities (for summer 2019) for high school students through collaboration with Saturday Academy, Apprenticeship in Science and Engineering program.`, 'article-para']
     ],
     "title" : "DeepMarket | News",
     "pageTitle": "News",
