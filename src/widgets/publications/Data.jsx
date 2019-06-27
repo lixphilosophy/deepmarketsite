d10b7b9412149aff634233fa73a5505f205b42d0
Martin <lixphilosophy@gmail.com>
2019-06-26 20:05:22 -0700

    fix data.jsx bug

--- src/widgets/publications/Data.jsx
+++ src/widgets/publications/Data.jsx
@@ -2,7 +2,7 @@ const Data = {
     "article": [
         [`DeepMarket Research Results`, `article-subtitle text-center font-sm-bold py-2`],
 
-        [`<i>S. Yerabolu, S. Gomena, E. Aryafar, and C. Joe-Wong</i>, “<a href="https//https://deepmarket.cs.pdx.edu/2019-SIGCOMM-DEMO.pdf" target="_blank">An Edge Computing Marketplace for Distributed Machine Learning</a>,” in Proceedings of ACM SIGCOMM (SIGCOMM Posters and Demos), Beijing, China, August 2019.`, 'article-para pb-2']
+        [`<i>S. Yerabolu, S. Gomena, E. Aryafar, and C. Joe-Wong</i>, “<a href="https//https://deepmarket.cs.pdx.edu/2019-SIGCOMM-DEMO.pdf" target="_blank">An Edge Computing Marketplace for Distributed Machine Learning</a>,” in Proceedings of ACM SIGCOMM (SIGCOMM Posters and Demos), Beijing, China, August 2019.`, 'article-para pb-2'],
         
         [`<i>S. Yerabolu, S. Kim, S. Gomena, X. Li, R. Patel, S. Bhise, and E. Aryafar,</i> “<a href="https://deepmarket.cs.pdx.edu/2019-ECOFEC-DeepMarket.pdf" target="_blank">DeepMarket: An Edge Computing Marketplace with Distributed TensorFlow Execution Capability</a>,” in IEEE ECOFEC (ECOnomics of Fog, Edge, and Cloud Computing) Workshop 2019, Paris, France, April 2019.`, 'article-para pb-2'],
 
