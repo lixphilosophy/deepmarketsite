4849d1c392b84f7367632674cbbc7cad62630336
Martin Li <lixphilosophy@gmail.com>
2020-04-13 04:24:26 -0400

    add news

--- src/widgets/news/Data.jsx
+++ src/widgets/news/Data.jsx
@@ -1,16 +1,21 @@
 const Data = {
     "article": [
-        [`<b>February 2020</b> - Payal gave a demo of DeepMarket at Intel in Hillsboro, OR. The demo day was organized by Oregon Mesa and was attended by high school and middle schools students (with high minority populations) as well as many Intel employees. We would like to thank Intel folks who visited our booth and gave useful comments on our project.`, 'article-para'],
 
-        [`<b>June, 2019</b> - Our demo is asccepted to ACM SIGCOMM. Sam will be presenting the demo in Beijing, China!`, 'article-para pt-3'],
+        [`<b>April 2020</b> - Our demo is accepted to IEEE ICDCS 2020! Thanks to all the undergraduate and Master’s students at PSU and Carnegie Mellon who contributed to the paper! We are looking forward to presenting a demo of DeepMarket at the ICDCS conference in July 2020!`, 'article-para'],
 
-        [`<b>May, 2019</b> - Congratulations to Soyoung for successfully defending her Master thesis on benchmarking DeepMarket.`, 'article-para pt-3'],
+        [`<b>March 2020</b> - More than 40 high school students applied to our summer intern position, which was announced in collaboration with Saturday Academy. We are in the process of interviewing candidates and will host one or two high school summer interns in our lab. The students will assist undergraduate and graduate students who are already working on DeepMarket.`, 'article-para pt-3'],
 
-        [`<b>April, 2019</b> - We presented our first paper (DeepMarket: An Edge Computing Marketplace with Distributed TensorFlow Execution Capability) at the First International IEEE Workshop on the Economics of Fog, Edge and Cloud Computing (ECOFEC 2019), in Paris, France. The paper was well received by the community and we are looking forward to expanding DeepMarket’s network to include other universities and research groups.`, 'article-para pt-3'],
+        [`<b>February 2020</b> - Payal gave a demo of DeepMarket at Intel in Hillsboro, OR. The demo day was organized by Oregon Mesa and was attended by high school and middle schools students (with high minority populations) as well as many Intel employees. We would like to thank Intel folks who visited our booth and gave useful comments on our project.`, 'article-para pt-3'],
+
+        [`<b>June 2019</b> - Our demo is asccepted to ACM SIGCOMM. Sam will be presenting the demo in Beijing, China!`, 'article-para pt-3'],
+
+        [`<b>May 2019</b> - Congratulations to Soyoung for successfully defending her Master thesis on benchmarking DeepMarket.`, 'article-para pt-3'],
+
+        [`<b>April 2019</b> - We presented our first paper (DeepMarket: An Edge Computing Marketplace with Distributed TensorFlow Execution Capability) at the First International IEEE Workshop on the Economics of Fog, Edge and Cloud Computing (ECOFEC 2019), in Paris, France. The paper was well received by the community and we are looking forward to expanding DeepMarket’s network to include other universities and research groups.`, 'article-para pt-3'],
         
-        [`<b>February, 2019</b> - Susham Yerabolu (CS Master student at Portland State University) was the first student who contributed to DeepMarket. He has accepted a job offer from Informatica (Austin, TX) and will be starting his new job in Feb 2019. We appreciate all his contributions to the project and wish him well in his career!`, 'article-para pt-3'],
+        [`<b>February 2019</b> - Susham Yerabolu (CS Master student at Portland State University) was the first student who contributed to DeepMarket. He has accepted a job offer from Informatica (Austin, TX) and will be starting his new job in Feb 2019. We appreciate all his contributions to the project and wish him well in his career!`, 'article-para pt-3'],
     
-        [`<b>August, 2018</b> - We hosted a high school student, Patrick Huang, from Southridge High School in our lab in summer 2018. Patrick contributed to several projects including the initial design of the DeepMarket website! We have posted new summer internship opportunities (for summer 2019) for high school students through collaboration with Saturday Academy, Apprenticeship in Science and Engineering program.`, 'article-para pt-3']
+        [`<b>August 2018</b> - We hosted a high school student, Patrick Huang, from Southridge High School in our lab in summer 2018. Patrick contributed to several projects including the initial design of the DeepMarket website! We have posted new summer internship opportunities (for summer 2019) for high school students through collaboration with Saturday Academy, Apprenticeship in Science and Engineering program.`, 'article-para pt-3']
     
     ],
     "title" : "DeepMarket | News",
